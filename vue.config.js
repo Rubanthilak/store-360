f67198e98d8d71fb112853e09082db33050c8323
Rubanthilak <rubanthilak.24@gmail.com>
2021-03-14 17:00:42 +0530

    ~Config changes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "Store 360";
+            return args;
+        })
+    },
     css: {
       loaderOptions: {
         sass: {
