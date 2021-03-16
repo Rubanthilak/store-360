4c0055bd7a4d07190832c37b053d669f2f0296e6
Rubanthilak <rubanthilak.24@gmail.com>
2021-03-16 16:21:43 +0530

    ~ testing failed

--- src/resources/database.js
+++
@@ -1,8 +0,0 @@
-import { initializeDatabase, synchronizeDatabase } from "./databaseHelper/databaseFunctions";
-
-var Database = function(){
-    var connection = initializeDatabase();
-    synchronizeDatabase(connection);
-}
-
-export default Database;
