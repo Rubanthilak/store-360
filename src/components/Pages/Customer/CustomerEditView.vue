6b940d91781dc48a2b54e82fde9b69c10c86783a
rubanthilak <rubanthilak@zoho.com>
2021-07-14 21:49:11 +0530

    ui fixes

--- src/components/Pages/Customer/CustomerEditView.vue
+++ src/components/Pages/Customer/CustomerEditView.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-      <top-bar>
+    <top-bar>
       <template #default>
         <div class="flex menu-bar">
           <div class="flex header">
@@ -12,7 +12,7 @@
             </div>
           </div>
           <div class="flex button-container">
-              <the-button label="Save" ></the-button>
+            <the-button label="Save"></the-button>
           </div>
         </div>
       </template>
@@ -20,27 +20,119 @@
     <div v-if="isLoading" class="container loading-wrapper">
       <loading-spinner></loading-spinner>
     </div>
-    <div v-else class="container">
+    <div v-else class="container wrapper">
       <div class="card">
         <div class="header">
-          <h1 class="title">Personal Details</h1>
+          <h1 class="title">Business Details</h1>
         </div>
-        <div class="content">
-            <p>Name</p>
-            <input type="text" v-model="customer.customerName">
+        <div class="row">
+          <div class="content">
+            <div>
+              <p class="label">Name</p>
+              <input type="text" v-model="customer.customerName" />
+            </div>
+          </div>
+          <div class="content">
+            <div>
+              <p class="label">Credits</p>
+              <input type="text" v-model="customer.customerCreditPoint" />
+            </div>
+          </div>
+          <div class="content">
+            <div>
+              <p class="label">GSTIN Number</p>
+              <input type="text" v-model="customer.customerGstinNumber" />
+            </div>
+          </div>
         </div>
       </div>
+      <hr />
       <div class="card">
         <div class="header">
           <h1 class="title">Billing Address</h1>
         </div>
-        <div class="content"></div>
+        <div class="row">
+          <div class="content">
+            <div>
+              <p class="label">Door Number</p>
+              <input type="text" v-model="customer.customerDoorNumber" />
+            </div>
+            <div>
+              <p class="label">Street Name</p>
+              <input type="text" v-model="customer.customerStreetName" />
+            </div>
+          </div>
+          <div class="content">
+            <div>
+              <p class="label">City Name</p>
+              <input type="text" v-model="customer.customerCityName" />
+            </div>
+            <div>
+              <p class="label">State Name</p>
+              <input type="text" v-model="customer.customerStateName" />
+            </div>
+          </div>
+          <div class="content">
+            <div>
+              <p class="label">Pincode</p>
+              <input type="text" v-model="customer.customerPincode" />
+            </div>
+          </div>
+        </div>
       </div>
-       <div class="card">
+      <hr />
+      <div class="card">
         <div class="header">
           <h1 class="title">Shipping Address</h1>
         </div>
-        <div class="content"></div>
+        <div class="row">
+          <div class="content">
+            <div>
+              <p class="label">Door Number</p>
+              <input type="text" v-model="customer.customerDoorNumber" />
+            </div>
+            <div>
+              <p class="label">Street Name</p>
+              <input type="text" v-model="customer.customerStreetName" />
+            </div>
+          </div>
+          <div class="content">
+            <div>
+              <p class="label">City Name</p>
+              <input type="text" v-model="customer.customerCityName" />
+            </div>
+            <div>
+              <p class="label">State Name</p>
+              <input type="text" v-model="customer.customerStateName" />
+            </div>
+          </div>
+          <div class="content">
+            <div>
+              <p class="label">Pincode</p>
+              <input type="text" v-model="customer.customerPincode" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div class="card">
+        <div class="header">
+          <h1 class="title">Contact Details</h1>
+        </div>
+        <div class="row">
+          <div class="content">
+            <div>
+              <p class="label">Phone Number</p>
+              <input type="text" v-model="customer.customerPhoneNumber" />
+            </div>
+          </div>
+          <div class="content">
+            <div>
+              <p class="label">Alternate Phone Number</p>
+              <input type="text" v-model="customer.customerPhoneNumber" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -55,12 +147,8 @@ export default {
     };
   },
   methods: {
-      validateInput(){
-
-      },
-      updateCustomerDetails(){
-
-      }
+    validateInput() {},
+    updateCustomerDetails() {},
   },
   async created() {
     this.isLoading = true;
@@ -74,6 +162,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+section {
+  margin-bottom: 50px;
+}
 
 .loading-wrapper {
   height: calc(100vh - 120px);
@@ -93,29 +184,47 @@ export default {
   margin-right: 10px;
 }
 
-.card{
-    background: var(--gray0);
-    border-radius: 5px;
-    box-shadow: 0px 0px 5px rgb(0,0,0,0.1);
-    padding:25px;
-    margin-bottom:10px;
+.wrapper {
+  background: var(--gray0);
+  border-radius: 5px;
+  box-shadow: 0px 0px 5px rgb(0, 0, 0, 0.1);
+  padding: 25px;
+  width: calc(90% - 50px);
+  min-width: 1150px;
 }
 
-.title{
-    font-family: var(--font-bold);
-    font-size: 24px;
+.card {
+  margin-bottom: 10px;
 }
 
-.content{
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
+.title {
+  font-family: var(--font-semibold);
+  font-size: 24px;
+  margin-bottom: 10px;
+  color: var(--gray6);
+}
 
-    input {
+.row {
+  display: flex;
+  flex-direction: row;
+  gap: 3rem;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  .label {
+    margin-left: 2px;
+    font-family: var(--font-medium);
+  }
+
+  input {
     padding: 8px 10px;
     border-radius: 5px;
     border: 2px solid var(--gray0);
-    box-shadow: inset 0px 0px 25px rgb(0,0,0,0.05);
+    box-shadow: inset 0px 0px 25px rgb(0, 0, 0, 0.05);
     font-family: var(--font-regular);
     background: var(--gray1);
     width: 320px;
@@ -124,11 +233,8 @@ export default {
       border: 2px solid var(--blue);
       outline: none;
       background: var(--gray0);
+      box-shadow: none;
     }
   }
-
 }
-
-
-
 </style>
