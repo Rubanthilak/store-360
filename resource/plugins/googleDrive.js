1b402cf782ddadb68256ced84687e43ca39974e2
rubanthilak <rubanthilak@zoho.com>
2021-08-04 13:01:03 +0530

    working on googledrive plugin

--- resource/plugins/googleDrive.js
+++ resource/plugins/googleDrive.js
@@ -1,25 +1,32 @@
-var _setImmediate2 = require('babel-runtime/core-js/set-immediate');
+var _setImmediate2 = require("babel-runtime/core-js/set-immediate");
 
 var _setImmediate3 = _interopRequireDefault(_setImmediate2);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
-const fs = require('fs');
-const {google} = require('googleapis');
+const fs = require("fs");
+const { google } = require("googleapis");
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+const SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = "token.json";
 
-// Load client secrets from a local file.
-fs.readFile('./credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Drive API.
-  authorize(JSON.parse(content), listFiles);
-});
+/**
+* @param {function} callback A function need to passed as callback to execute after authentication.
+*/
+function initiateAuthentication(callback) {
+  // Load client secrets from a local file.
+  fs.readFile("./credentials.json", (err, content) => {
+    if (err) return console.log("Error loading client secret file:", err);
+    // Authorize a client with credentials, then call the Google Drive API.
+    authorize(JSON.parse(content), callback);
+  });
+}
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -28,9 +35,12 @@ fs.readFile('./credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
@@ -48,7 +58,7 @@ function authorize(credentials, callback) {
  */
 function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
+    access_type: "offline",
     scope: SCOPES,
   });
   var code = function code() {
@@ -56,96 +66,82 @@ function getAccessToken(oAuth2Client, callback) {
   };
   code().then((code) => {
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
+      if (err) return console.error("Error retrieving access token", err);
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
         if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
+        console.log("Token stored to", TOKEN_PATH);
       });
       callback(oAuth2Client);
     });
   });
 }
 
-/**
- * Lists the names and IDs of up to 10 files.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listFiles(auth) {
-  const drive = google.drive({version: 'v3', auth});
-  drive.files.list({
-    pageSize: 10,
-    fields: 'nextPageToken, files(id, name)',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const files = res.data.files;
-    if (files.length) {
-      console.log('Files:');
-      files.map((file) => {
-        console.log(`${file.name} (${file.id})`);
+function authorizeApp(url) {
+  return new Promise(function(resolve, reject) {
+    try {
+      // Changes for GAuth wrt. bug user is not able to take backup on GDrive.
+      // Creating two browser window to authenticate user.
+      // GAuth is not working directly with user-agent.
+      // Workaround: First request is without user-agent, once we load it without user-agent then again we are trying to load it with user-agent.
+      var browserWindowParams = {
+        useContentSize: true,
+        center: true,
+        show: false,
+        resizable: false,
+        autoHideMenuBar: true,
+        alwaysOnTop: false,
+      };
+
+      var BrowserWindow = require("electron").remote.BrowserWindow;
+
+      var win = new BrowserWindow(
+        browserWindowParams || { "use-content-size": true }
+      );
+      win.webContents.on("did-finish-load", function() {
+        var browserWindowParams1 = {
+          useContentSize: true,
+          center: true,
+          show: true,
+          resizable: false,
+          autoHideMenuBar: true,
+          alwaysOnTop: true,
+        };
+        var win1 = new BrowserWindow(
+          browserWindowParams1 || { "use-content-size": true }
+        );
+        win.close();
+        win1.loadURL(url, { userAgent: "Chrome" });
+        win1.on("closed", function() {
+          reject(new Error("User closed the window"));
+        });
+        win1.on("page-title-updated", function() {
+          (0, _setImmediate3.default)(function() {
+            var title = win1.getTitle();
+            if (title.startsWith("Denied")) {
+              reject(new Error(title.split(/[ =]/)[2]));
+              win1.removeAllListeners("closed");
+              console.log(new Error("User closed the window"));
+              win1.close();
+            } else if (title.startsWith("Success")) {
+              resolve(title.split(/[ =]/)[2]);
+              win1.removeAllListeners("closed");
+              win1.close();
+              console.log(
+                "Started Backup to Google Drive. Please continue with your work."
+              );
+            }
+          });
+        });
       });
-    } else {
-      console.log('No files found.');
+      win.loadURL(url);
+    } catch (ex) {
+      reject(
+        new Error("Unable to load the window. Please contact Store360 team.")
+      );
     }
   });
 }
 
-
-function authorizeApp(url) {
-    return new Promise(function (resolve, reject) {
-        try {
-            // Changes for GAuth wrt. bug user is not able to take backup on GDrive.
-            // Creating two browser window to authenticate user.
-            // GAuth is not working directly with user-agent.
-            // Workaround: First request is without user-agent, once we load it without user-agent then again we are trying to load it with user-agent.
-            var browserWindowParams = {
-                useContentSize: true,
-                center: true,
-                show: false,
-                resizable: false,
-                autoHideMenuBar: true,
-                alwaysOnTop: false
-            };
-
-            var BrowserWindow = require('electron').remote.BrowserWindow;
-
-            var win = new BrowserWindow(browserWindowParams || { 'use-content-size': true });
-            win.webContents.on('did-finish-load', function () {
-                var browserWindowParams1 = {
-                    useContentSize: true,
-                    center: true,
-                    show: true,
-                    resizable: false,
-                    autoHideMenuBar: true,
-                    alwaysOnTop: true
-                };
-                var win1 = new BrowserWindow(browserWindowParams1 || { 'use-content-size': true });
-                win.close();
-                win1.loadURL(url, { userAgent: 'Chrome' });
-                win1.on('closed', function () {
-                    reject(new Error('User closed the window'));
-                });
-                win1.on('page-title-updated', function () {
-                    (0, _setImmediate3.default)(function () {
-                        var title = win1.getTitle();
-                        if (title.startsWith('Denied')) {
-                            reject(new Error(title.split(/[ =]/)[2]));
-                            win1.removeAllListeners('closed');
-                            console.log(new Error('User closed the window'));
-                            win1.close();
-                        } else if (title.startsWith('Success')) {
-                            resolve(title.split(/[ =]/)[2]);
-                            win1.removeAllListeners('closed');
-                            win1.close();
-                            console.log('Started Backup to Google Drive. Please continue with your work.');
-                        }
-                    });
-                });
-            });
-            win.loadURL(url);
-        } catch (ex) {
-            reject(new Error('Unable to load the window. Please contact Store360 team.'));
-        }
-    });
-}
+export default initiateAuthentication;
